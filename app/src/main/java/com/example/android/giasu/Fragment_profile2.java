dc9950f5c8dc485d6a9e7600c967d569d1602590
Gia Sư <https://github.com/TranQuan289/Gia-S->
2021-11-07 08:30:17 +0700

    Q4T

--- app/src/main/java/com/example/android/giasu/Fragment_profile2.java
+++ app/src/main/java/com/example/android/giasu/Fragment_profile2.java
@@ -4,29 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-public class Fragment_profile extends Fragment {
-    TextView logout;
-
+public class Fragment_profile2 extends Fragment {
+    ImageView back;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.activity_profile,container,false);
-        logout = v.findViewById(R.id.txt_proflie);
-
-        logout.setOnClickListener(new View.OnClickListener() {
+        View v = inflater.inflate(R.layout.activity_generalinformation,container,false);
+        back = v.findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                replaceFragment4(new Fragment_profile2());
+                replaceFragment4(new Fragment_profile());
             }
         });
-
         return v;
     }
     private void replaceFragment4(Fragment fragment){

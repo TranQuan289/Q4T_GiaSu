44714585fc7e1d47104c186401b6c2806b18117d
Tran Quân <anhquan28092001@gmail.com>
2022-01-06 15:24:24 +0700

    commit

--- app/src/main/java/com/example/android/create/CreateTeacherFragment.java
+++ app/src/main/java/com/example/android/create/CreateTeacherFragment.java
@@ -3,6 +3,7 @@ package com.example.android.create;
 import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -22,8 +23,20 @@ public class CreateTeacherFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.activity_createteacher,container,false);
+        View v = inflater.inflate(R.layout.activity_createteacher, container, false);
         Button btn_next;
+        Button btn_day1 = v.findViewById(R.id.btn_day1);
+        Button btn_day2 = v.findViewById(R.id.btn_day2);
+        Button btn_day3 = v.findViewById(R.id.btn_day3);
+        Button btn_day4 = v.findViewById(R.id.btn_day4);
+        Button btn_day5 = v.findViewById(R.id.btn_day5);
+        Button btn_day6 = v.findViewById(R.id.btn_day6);
+        Button btn_day7 = v.findViewById(R.id.btn_day7);
+        btn_day1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
         btn_next = v.findViewById(R.id.btn_next);
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +52,7 @@ public class CreateTeacherFragment extends Fragment {
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.dialog_post);
         Window window = dialog.getWindow();
-        if(window == null){
+        if (window == null) {
             return;
         }
         window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
@@ -47,12 +60,12 @@ public class CreateTeacherFragment extends Fragment {
         WindowManager.LayoutParams layoutParams = window.getAttributes();
         layoutParams.gravity = gravity;
         window.setAttributes(layoutParams);
-        if(Gravity.BOTTOM == gravity){
+        if (Gravity.BOTTOM == gravity) {
             dialog.setCancelable(true);
-        }else {
+        } else {
             dialog.setCancelable(false);
         }
         dialog.show();
     }
-    }
+}
 

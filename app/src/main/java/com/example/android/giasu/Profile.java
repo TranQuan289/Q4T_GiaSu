869e0b32e15190ce1c309097786192f864ed1365
Tran Quân <anhquan28092001@gmail.com>
2022-01-08 15:37:04 +0700

    commit

--- app/src/main/java/com/example/android/giasu/Profile.java
+++ app/src/main/java/com/example/android/giasu/Profile.java
@@ -5,27 +5,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.android.Find.Fragment_Find;
 import com.example.android.mess.MessFragment2;
 
 public class Profile extends Fragment {
     Button button;
-
+    ImageView img_back;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_profile1,container,false);
-        button=(Button) v.findViewById(R.id.traodoi);
-
+        button=(Button) v.findViewById(R.id.btn_contact);
+        img_back = v.findViewById(R.id.img_back);
+        img_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment4(new Fragment_Find());
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                replaceFragment4(new MessFragment2());
+                replaceFragment4(new Fragment_email());
             }
         });
 
